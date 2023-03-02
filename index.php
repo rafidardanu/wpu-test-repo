ea5cfcd6b18573dfd9fdaf5b8497b403c3313f84
rafidardanu <rescuephones2016@gmail.com>
2023-03-02 20:43:04 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>coba aja ni</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas vero illum iste commodi autem optio dolor quaerat id expedita voluptatibus, pariatur sapiente suscipit, atque corporis nostrum illo assumenda quibusdam.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
